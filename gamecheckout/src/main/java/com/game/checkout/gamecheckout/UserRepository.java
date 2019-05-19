f576bd3421b989e21af031f95a55d9f5adc55ebe
Alex Louden <alexlouden29@yahoo.com>
2019-05-19 08:57:36 -0700

    Moved backend to new git repo

--- gamecheckout/src/main/java/com/game/checkout/gamecheckout/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.game.checkout.gamecheckout;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long>{}
