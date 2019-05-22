9bea41a68a6868d3b66c6b9cb6d4bd1e26c41928
Tuan Le <tle@arrowheadgrp.com>
2019-05-22 15:05:20 -0700

    added game list component

---
+++ src/app/model/game.ts
@@ -0,0 +1,14 @@
+export class Game {
+    gameId: string;
+    title: string;
+    playerCount: string;
+    playTime: string;
+    age: number;
+    complexity: string;
+    designer: string;
+    publisher: string;
+    yearPublished: number;
+    rating: string;
+    dateAdded: Date;
+    lastModified: Date;
+}
