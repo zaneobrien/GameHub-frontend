1438a0ca306db9416a9152d5ba302a9e6f43973f
Tuan Le <tle@arrowheadgrp.com>
2019-06-12 13:21:54 -0700

    code cleanup

--- src/app/model/game-detail/game-detail.component.ts
+++ src/app/model/game-detail/game-detail.component.ts
@@ -21,18 +21,17 @@ export class GameDetailComponent implements OnInit {
    }
 
   public show(): void {
-	console.log('status = ' + this.game.status);
-	if ('AVAILABLE' === this.game.status) {
-		this.showBtn = true;
-	}
-	console.log('showBtn = ' + this.showBtn);
+    console.log('status = ' + this.game.status);
+    if ('AVAILABLE' === this.game.status) {
+        this.showBtn = true;
+    }
+    console.log('showBtn = ' + this.showBtn);
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-		this.gameService.findById(params.gameId).subscribe(data => { this.game = data;
-																     this.show(); });
-	});
+       this.gameService.findById(params.gameId).subscribe(data => { this.game = data; this.show(); });
+    });
   }
 
 }
